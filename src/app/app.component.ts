02278d3eae0b64eb96fdbe1b9af78de533e4f5ca
Vishu7512 <vishalgupta7684@gmail.com>
2024-08-27 18:36:26 +0530

    add some files of seller and update UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component , OnInit} from '@angular/core';
-import{Router} from '@angular/router'
+import { Router } from '@angular/router'
+import { ProductsService } from './services/products.service';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +9,31 @@ import{Router} from '@angular/router'
 })
 export class AppComponent implements OnInit {
   title = 'E-commerce-application';
-  menuType :String='default';
-  constructor(private router:Router) { }
+  menuType: String = 'default';
+  trendyProducts = []
+  constructor(private router: Router, private productsService: ProductsService) { }
   
   ngOnInit(): void {
 
-    this.router.events.subscribe((val:any) => {
-      if (val.url) {
-        if (localStorage.getItem('seller') && val.url.includes('seller')) {
-          console.warn("in seller")
-          this.menuType ='seller'
-        }else{
-          console.warn("out seller")
-          this.menuType ='default'
-        }
-      }
+    this.productsService.getData().subscribe((data: any) => {
+      this.trendyProducts = data
+      console.log(data);
+
+      //   this.router.events.subscribe((val:any) => {
+      //     if (val.url) {
+      //       if (localStorage.getItem('seller') && val.url.includes('home')) {
+      //         console.warn("in seller")
+      //         this.menuType ='seller'
+      //       }else{
+      //         console.warn("out seller")
+      //         this.menuType ='default'
+      //       }
+      //     }
+      //   })
+      // }
+    
     })
-  }
 }
+}
+
+  
